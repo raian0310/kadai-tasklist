6ac63fbcd8b3b84619c8171b186d9e2cebdea59e
raian <maxrailai@softbank.ne.jp>
2020-09-07 04:02:05 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_024051) do
+ActiveRecord::Schema.define(version: 2020_09_07_035216) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_024051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.text "status"
   end
 
 end
